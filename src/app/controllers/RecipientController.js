47dc6e5590b8e06b850f4909eda3dd5dba22650e
Bruno Girome <brunogirome@gmail.com>
2020-02-16 14:43:05 -0300

    Cofigurado schema do Yup do Recipient e Session

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
@@ -32,8 +33,23 @@ class RecipientController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      recipientName: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipCode: Yup.string()
+        .length(8)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const {
-      id,
       recipientName,
       street,
       number,
@@ -44,7 +60,6 @@ class RecipientController {
     } = await Recipient.create(req.body);
 
     return res.json({
-      id,
       recipientName,
       street,
       number,
